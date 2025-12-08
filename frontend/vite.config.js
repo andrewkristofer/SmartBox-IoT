4e88b427379776894ce019042439b01d0c16075e
sharifmasyhur <sharifalhadyyad@gmail.com>
2025-12-08 12:32:12 +0700

    fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cssnano from 'cssnano'
 
+// https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  css: {
-    transformerOptions: {
-      lightningcss: {
-        minify: false,   // 🚫 Matikan minifier LightningCSS (penyebab error)
-      },
-    },
-    postcss: {
-      plugins: [
-        cssnano({ preset: 'default' })  // ✔ pakai cssnano sebagai pengganti
-      ]
-    }
-  },
-  build: {
-    cssMinify: false  // ✔ pastikan Vite tidak coba minify ulang
-  }
 })
