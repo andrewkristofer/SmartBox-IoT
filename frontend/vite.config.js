a6acf708f7ba64c1cc52e8136ac2bf6b26a90cbe
sharifmasyhur <sharifalhadyyad@gmail.com>
2025-12-08 12:40:47 +0700

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
+
   css: {
     transformer: 'postcss',
-    minify: false,
+    minify: false, // <— memastikan lightningcss TIDAK dipakai
+    postcss: './postcss.config.js',
   },
+
+  build: {
+    cssMinify: false, // <— ini yang paling penting
+  },
+
   plugins: [react()],
 })
